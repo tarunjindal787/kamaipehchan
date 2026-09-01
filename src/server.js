7edde85c5c4351dba8bd1da346679bb13381c2d9
Tarun Jindal <jindaltarun186@gmail.com>
2026-09-01 11:51:02 +0530

    Feat: Complete the worker journey with Onboarding UI and Lender Dashboard UI

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const app = express();
 // Security: basic headers & payload size limit
 app.use((_req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   next();
 });
